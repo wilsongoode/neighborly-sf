8bc7f8985501590a8425e09c654067d1a24505c7
Wilson Goode <wilsongoode97@gmail.com>
2019-08-20 16:59:54 -0700

    added organizations

---
+++ database/seeds/OrganizationSeeder.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| OrganizationSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class OrganizationSeeder {
+  async run () {
+    
+
+  }
+}
+
+module.exports = OrganizationSeeder
