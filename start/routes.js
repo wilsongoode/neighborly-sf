70897f6e0364e8246e712fcd1bdd0c5c57d8d2b0
Randall Lee Springer <rspringer@rollins.edu>
2019-08-21 16:30:00 -0700

    Added in routes, routes for after user logged in commented out

--- start/routes.js
+++ start/routes.js
@@ -36,6 +36,7 @@ Route.on("/").render("index");
 
 Route.on("/welcome").render("welcome");
 
+// Route for before the user logged in
 Route.group(() => {
   Route.get('/register', 'RegisterController.create').as('register.create')
   Route.post('/register', 'RegisterController.store').as('register.store').validator('Register')
@@ -43,6 +44,16 @@ Route.group(() => {
   Route.get('/login', 'LoginController.create').as('login.create')
   Route.post('/login', 'LoginController.store').as('login.store')
 }).middleware(['guest'])
+
+// Route for after the user logged in
+// Route.group(() => {
+//   Route.get('/todos', 'TodoController.index').as('todos.index')
+//   Route.post('/todos', 'TodoController.store').as('todos.store').validator('StoreTodo')
+//   Route.get('/todos/:id/edit', 'TodoController.edit').as('todos.edit')
+//   Route.patch('/todos/:id', 'TodoController.update').as('todos.update').validator('UpdateTodo')
+//   Route.delete('/todos/:id', 'TodoController.destroy').as('todos.delete')
+//   Route.post('/logout', 'LoginController.destroy').as('logout')
+// }).middleware(['auth'])
 
 //logout
 Route.get("/logout", async ({ auth, response }) => {
